c98062a80b8438cb7811768c5ba9b388ad71bb34
PolinaRuzanova <94633834+PolinaRuzanova@users.noreply.github.com>
2022-04-28 00:14:41 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,6 +5,61 @@
 template<typename T>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на связанном списке
+  private:
+    struct ITEM {
+        T value;
+        ITEM* next;
+        ITEM* prev;
+    };
+    ITEM* head;
+    ITEM* tail;
+    TPQueue::ITEM* create(const T& value) {
+        ITEM* item = new ITEM;
+        item->value = value;
+        item->next = item->prev = nullptr;
+        return item;
+    }
+
+public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    void push(const T& value) {
+        ITEM* temp = head;
+        ITEM* item = create(value);
+        while (temp && temp->value.prior >= value.prior)
+            temp = temp->next;
+        if (!temp && head) {
+            tail->next = item;
+            item->prev = tail;
+            tail = item;
+        }
+        else if (!temp && !head) {
+            head = tail = item;
+        }
+        else if (!temp->prev) {
+            head->prev = item;
+            item->next = head;
+            head = item;
+        }
+        else {
+            temp->prev->next = item;
+            item->prev = temp->prev;
+            item->next = temp;
+            temp->prev = item;
+        }
+    }    
+    T pop() {
+        if (!head || !tail)
+            throw std::string("The queue is empty!");
+        ITEM* temp = head->next;
+        if (temp)
+            temp->prev = nullptr;
+        T value = head->value;
+        delete head;
+        head = temp;
+        if (!head)
+            tail = nullptr;
+        return value;
+    }
 };
 
 struct SYM {
